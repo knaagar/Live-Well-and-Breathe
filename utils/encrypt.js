d26536a0c6e5d00794b42b3016f5e8c2bfb4e2f4
LeafKn <90194661+knaagar@users.noreply.github.com>
2021-10-10 22:23:27 +0530

    Update encrypt.js

--- utils/encrypt.js
+++ utils/encrypt.js
@@ -1,1 +1,43 @@
+const bcrypt = require("bcrypt");
 
+/**
+ * Generates a salt for a hash
+ * 
+ * @param {string} s - salt input
+ * 
+ * @returns {string} - salt output
+ */
+const genSalt = async s => {
+    return await bcrypt.genSalt(s).catch(err => { throw err; });
+};
+
+/**
+ * Hashes a string
+ * 
+ * @param {string} h - to hash
+ * @param {string} s - salt
+ * 
+ * @returns {string} - hashed string
+ */
+const genHash = async (h, s) => {
+    return await bcrypt.hash(h, s).catch(err => { throw err; });
+};
+
+/**
+ * Validate a hash
+ * 
+ * @param {string} t - text to check
+ * @param {string} h - original hash
+ * 
+ * @returns {boolean} - does the text match the hash
+ */
+const checkHash = async (t, h) => {
+    return await bcrypt.compare(t, h).catch(err => { throw err; });
+};
+
+module.exports = {
+    genSalt : genSalt,
+    genHash : genHash,
+    checkHash : checkHash
+};
+ 
